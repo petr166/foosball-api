44fad7da8898ff4b3f70c5c7d21d77680c0ed937
Petru Birzu <petru.birzu96@gmail.com>
2019-05-05 19:07:26 +0200

    added tournamentInvitation.model

--- models/index.ts
+++ models/index.ts
@@ -1,4 +1,5 @@
 import User from './user.model';
 import Tournament from './tournament.model';
+import TournamentInvitation from './tournamentInvitation.model';
 
-export { User, Tournament };
+export { User, Tournament, TournamentInvitation };
---
+++ models/tournamentInvitation.model.ts
@@ -0,0 +1,35 @@
+import { Schema, model, Document } from 'mongoose';
+
+import { IUser } from './user.model';
+import { ITournament } from './tournament.model';
+
+export interface ITournamentInvitation extends Document {
+  id: string;
+  tournament: string | ITournament;
+  user: string | IUser;
+}
+
+const tournamentInvitationSchema = new Schema(
+  {
+    tournament: {
+      type: Schema.Types.ObjectId,
+      ref: 'Tournament',
+      required: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+tournamentInvitationSchema.set('toObject', { getters: true, virtuals: true });
+
+const TournamentInvitation = model<ITournamentInvitation>(
+  'TournamentInvitation',
+  tournamentInvitationSchema
+);
+
+export default TournamentInvitation;
