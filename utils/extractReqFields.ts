2937361b0750265fa7e10c05bbadd588bdd0c373
Petru Birzu <petru.birzu96@gmail.com>
2019-05-06 13:55:14 +0200

    updated extractReqFields

--- utils/extractReqFields.ts
+++ utils/extractReqFields.ts
@@ -1,26 +1,34 @@
-// const extractReqFieldsDeep = (selections: any[]): { root: string } => {
-//   const deeperSelections: any = {};
+const extractReqFieldsDeep = (selections: any[], key: string = ''): string => {
+  let deeperSelections = key ? key + ' ' : '';
+  const prefix = key ? key + '.' : '';
 
-//   selections.forEach(({ name: { value }, selectionSet }) => {
-//     if (selectionSet) {
-//       deeperSelections[value] = extractReqFieldsDeep(selectionSet.selections);
-//     }
-//   });
+  selections.forEach(({ name: { value }, selectionSet }) => {
+    if (selectionSet) {
+      deeperSelections += extractReqFieldsDeep(
+        selectionSet.selections,
+        prefix + value
+      );
+    }
+  });
 
-//   return {
-//     root: selections
-//       .filter((v: any) => !v.selectionSet)
-//       .map(({ name: { value } }: any) => value)
-//       .join(' '),
-//     ...deeperSelections,
-//   };
-// };
+  return (
+    prefix +
+    '_id' +
+    ' ' +
+    selections
+      .filter((v: any) => !v.selectionSet)
+      .map(({ name: { value } }: any) => prefix + value)
+      .join(' ') +
+    ' ' +
+    deeperSelections
+  );
+};
 
 // extract requested fields from grapql query
-export default (info: any): { root: string } => {
+export default (info: any): string => {
   const {
     selectionSet: { selections },
   } = info.fieldNodes[0];
 
-  return selections.map(({ name: { value } }: any) => value).join(' ');
+  return extractReqFieldsDeep(selections);
 };
